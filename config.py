8b2cf2e11020915f5cf2b4720df482493b0ef40a
keeve101 <miyamii.x@gmail.com>
2025-01-17 17:15:45 +0800

    feat: add code to download flores_plus to datasets storage

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 STORAGE_DIR = "/run/media/keith/a1cdd4e6-1c93-4b0a-8e04-b49b3dfb68d9"
 STORAGE_DIR_DATA_FLEURS = STORAGE_DIR + "/data/fleurs"
+STORAGE_DIR_DATA_FLORES_PLUS = STORAGE_DIR + "/data/flores_plus"
 STORAGE_DIR_MODEL_WHISPER = STORAGE_DIR + "/models/whisper"
 STORAGE_DIR_MODEL_MMS_1B_ALL = STORAGE_DIR + "/models/mms-1b-all"
